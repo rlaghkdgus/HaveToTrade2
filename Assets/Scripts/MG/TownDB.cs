491122fc05b4193246eddf31c12aaec6039b299d
wjsals <wjsals9384@gmail.com>
2025-11-08 22:08:23 +0900

    Fix2

--- Assets/Scripts/MG/TownDB.cs
+++ Assets/Scripts/MG/TownDB.cs
@@ -13,7 +13,7 @@ public class TownDB : ScriptableObject
 
     public List<GameObject> TownPrefabs; // 마을 프리팹
 
-    public List<GameObject> RoadPrefabs_F; // 앞
-    public List<GameObject> RoadPrefabs_M; // 중간
-    public List<GameObject> RoadPrefabs_B; // 뒤
+    public GameObject RoadPrefabs_F; // 앞
+    public GameObject RoadPrefabs_M; // 중간
+    public GameObject RoadPrefabs_B; // 뒤
 }
